70297f06f06ec644a21be75d6b4cc310932c45d3
Kurucz János <kuruczjanoshivatalos@gmail.com>
2023-02-11 13:04:55 +0100

    Finomítás

--- src/app.js
+++ src/app.js
@@ -7,3 +7,22 @@
 * Github: https://github.com/KuruczJanos/idomunka.git
 * Licenc: GNU GPL
 */
+const l_mElem = document.querySelector('#l_m');
+const f_nElem = document.querySelector('#f_n');
+const n_rpmElem = document.querySelector('#n_rpm');
+const calcButton = document.querySelector('#calcButton');
+const worktimeElem = document.querySelector('#worktime');
+
+calcButton.addEventListener('click',() => {
+    let l_m = Number (l_mElem.value);
+    let f_n = Number (f_nElem.value);
+    let n_rpm = Number (n_rpmElem.value);
+    let worktime = calcworktime (l_m, f_n, n_rpm);
+    console.log(worktime);
+    worktimeElem.value = worktime + " perc";
+});
+
+function calcworktime(l_m, f_n, n_rpm){
+    let worktime = (l_m)/(f_n*n_rpm)
+    return worktime;
+}
